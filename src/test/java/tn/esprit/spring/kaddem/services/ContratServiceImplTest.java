8ee03e2708ae05785fc586990e601ed00decc2d2
lamouchi-ale <lamouchiale@gmail.com>
2024-11-14 15:39:03 +0100

    test unitaire

--- src/test/java/tn/esprit/spring/kaddem/services/ContratServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/services/ContratServiceImplTest.java
@@ -1,10 +1,15 @@
 package tn.esprit.spring.kaddem.services;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.spring.kaddem.entities.Contrat;
 import tn.esprit.spring.kaddem.entities.Etudiant;
 import tn.esprit.spring.kaddem.entities.Specialite;
@@ -15,7 +20,9 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
 class ContratServiceImplTest {
 
     @Mock
